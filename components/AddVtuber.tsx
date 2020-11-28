5b46889a8c9c52450eee1087eae9e78e2891c9cd
naotaroimo <noymaeyama@gmail.com>
2020-11-28 23:56:58 +0900

    end:nextjs with typescript:21

--- components/AddVtuber.tsx
+++ components/AddVtuber.tsx
@@ -1,8 +1,12 @@
 import { Formik, Form, Field } from 'formik';
 import { Button, Box, FormGroup, TextField } from '@material-ui/core';
 import axios from 'axios';
+import useSWR, { mutate, trigger } from 'swr';
 
 const AddVtuber = () => {
+    //mutate用に先にデータフェッチをしておく
+    const { data } = useSWR('http://localhost:4001/persons');
+
     return (
         <div>
 
@@ -14,10 +18,17 @@ const AddVtuber = () => {
                     }
                 }
                 onSubmit={async (values, formikHelpers) => {
+                    //post前にフェッチしているデータに対し更新をいれる。
+                    //第２引数にフェッチデータdataとformikでバインドしてるvaluesを連結させる
+                    //ただし,
+                    //revalidate=false⇒SWRでのデータ再取得はaxios.postの後にする
+                    mutate('http://localhost:4001/persons', [...data, values], false);
+
                     await axios.post('http://localhost:4001/persons',values);
                    
                     formikHelpers.resetForm();//フォームリセット
-                    alert(` input data '${values.name}' and '${values.details}' added ! `)
+                    // alert(` input data '${values.name}' and '${values.details}' added ! `)
+                    trigger('http://localhost:4001/persons');
                 }}
             >
 
